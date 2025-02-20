13f3e1cb87f1d95f85c8f79ef396296b607a5bbc
Nxlogix <165151602+Nxlogix@users.noreply.github.com>
2025-02-19 21:11:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Desarrollo dev
         </a>
       </header>
     </div>
